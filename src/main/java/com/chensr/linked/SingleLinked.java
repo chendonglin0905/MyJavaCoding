ba7920e495056061139ec769131632d31f58a5ec
chendonglin <chendonglin@daojia-inc.com>
2019-03-11 09:21:53 +0800

    remove class file

--- src/main/java/com/chensr/linked/SingleLinked.java
+++ src/main/java/com/chensr/linked/SingleLinked.java
@@ -42,10 +42,6 @@ public class SingleLinked<T> implements List<T> {
             head = singleLinkedNode;
             tail = singleLinkedNode;
             // if element size 1
-        } else if (head == tail) {
-            head.setNext(singleLinkedNode);
-            tail = singleLinkedNode;
-            // if element size > 1
         } else {
             tail.setNext(singleLinkedNode);
             tail = singleLinkedNode;
